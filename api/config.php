0568d2ad9d32681b83324d294a15aac6e986a71d
Carlosmarin <carlosdanielmarin347@gmail.com>
2026-04-22 13:59:00 -0500

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,14 +1,11 @@
 FROM php:8.2-apache
 
-# Instalar PostgreSQL y extensiones necesarias
+# Instalar PostgreSQL y extensiones
 RUN apt-get update && apt-get install -y libpq-dev \
-    && docker-php-ext-install pdo_pgsql pgsql mysqli
+    && docker-php-ext-install pdo_pgsql pgsql
 
 # Copiar archivos
 COPY . /var/www/html/
-
-# Habilitar mod_rewrite
-RUN a2enmod rewrite
 
 # Configurar permisos
 RUN chown -R www-data:www-data /var/www/html
